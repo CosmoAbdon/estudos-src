f00e73059bcffa2d8f69082b5f52d2d5feb03a87
CosmoAbdon <cosmo.webp2@gmail.com>
2013-08-20 10:17:04 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,44 @@
+<html>
+
+
+
+	<head>
+	<title> -  </title>
+	
+	</head>
+
+
+
+<body bgcolor="black" text="white">
+
+	<table>
+<tr>
+<img src=img/header.png>
+</tr>   </table>
+
+<br><br><br><br><br><br><br><br> <!-- Espaçamento entre header e painel ! -->
+
+<form>
+<table align="center">
+	
+<!-- <div id="login"> -->     <tr><td><h1>          Login</h1></td></tr>
+	
+<tr><tr><td>- - - - - - - - - - - - </td><td>- - - - - - - - - - - - - - - - -</td></tr>
+<!-- <div id="usu"> --> <td><label>| Usuário </label></td><td> <input type="text" name="nick"> |</td> <!--</div> -->
+</tr>
+<tr><td>- - - - - - - - - - - - </td><td>- - - - - - - - - - - - - - - - -</td></tr>
+
+<tr><tr><td>- - - - - - - - - - - - </td><td>- - - - - - - - - - - - - - - - -</td></tr>
+<!-- <div id="senha"> --> <td><label>| Senha </label></td><td> <input type="password" name="senha"> | </td> <!-- </div> -->
+</tr><tr><td>- - - - - - - - - - - - </td><td>- - - - - - - - - - - - - - - - -</td></tr>
+	
+<br><br>
+
+<tr><td><input type="submit"  value="Enviar"></td><td>          <input type="reset"  value="Limpar campos"> </td></tr>
+<!-- </div> -->
+</table>
+</form>
+
+</body>
+
+</html>
