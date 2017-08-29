0b0aec65010ad5cda7b652eb2947ad33b0c03932
Cosmo Abdon <cosmo.webp2@gmail.com>
2017-08-29 16:57:53 -0300

    Create evelyn.py

---
+++ evelyn.py
@@ -0,0 +1,8 @@
+import math
+a =float(input(' Digite o valorem pés'))
+c1= a*12
+print('Valor em polegadas é:', c1)
+c2= a*3
+print(' Valor em jardas é:', c2)
+c3=c2*1760
+print('Valor em milhas',c3)
